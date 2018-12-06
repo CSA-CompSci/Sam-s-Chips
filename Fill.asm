358861c78e8d6049c08291c0b894e9abb5bf56d2
CSA-SBN <43680311+CSA-SBN@users.noreply.github.com>
2018-12-06 10:49:32 -0600

    Update Fill.asm

--- Fill.asm
+++ Fill.asm
@@ -13,48 +13,48 @@
 
 // Put your code here.
 (RESTART)
-@SCREEN
+@SCREEN //start the screen
 D=A
-@0
+@0 //screen start is lodged at RAM0
 M=D
 
 @KBD
 D=M
 @BLACK
-D;JGT
+D;JGT //jump if any kbd keys are being pressed, display black
 @WHITE
-D;JEQ
+D;JEQ //if any kbd are not pressed, display white
 
 @KBD
 0;JMP
 (BLACK)
 @1
-M=-1
+M=-1 //fill screen with m=-1, all black
 @CHANGE
 0;JMP
 (WHITE)
 @1
-M=0
+M=0 //fill screen with m=0, all white
 @CHANGE
 0;JMP
 
-@1
-D=M
+@1 
+D=M // look in RAM1 to see if the screen is black or white
 
 @0
-A=M
-M=D
+A=M //find the address of the pixel about to be filled
+M=D //display on that pixel whatever is inside RAM0 
 
-@0
-D=M+1
+@0 
+D=M+1 //moving over to the next pixel
 @KBD
-D=A-D
+D=A-D //whatever was displayed on the previous pixel, display the same color
 
 @0
-M=M+1
+M=M+1 //move over to the next pixel, display the same color
 A=M
 
-@CHANGE
-0;JGT
+@CHANGE 
+0;JGT //if the whole screen is black (a=0), exit the program
 @RESTART
-0;JMP
+0;JMP //otherwise restart
